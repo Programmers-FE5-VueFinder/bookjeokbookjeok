20a25b4933df52c3f2bd8906def9f2535c1da1c5
SeolWolWha <leeyoon97@naver.com>
2025-06-08 14:43:13 +0900

    💄 style: 이미지 적용 범위 overflow hidden 처리

--- src/components/component/BookCard.tsx
+++ src/components/component/BookCard.tsx
@@ -2,7 +2,7 @@ export default function BookCard() {
   return (
     <>
       <div className="relative h-[440px] w-[278px] flex-col justify-center rounded-2xl border-1 border-red-500 text-center shadow-xl">
-        <div className="h-[247px] w-[278px] content-center justify-center rounded-t-2xl border-1 text-center">
+        <div className="h-[247px] w-[278px] content-center justify-center overflow-hidden rounded-t-2xl border-1 text-center">
           <span>이미지 에리어</span>
         </div>
         <div className="mx-[7.99px] text-start">
@@ -10,7 +10,8 @@ export default function BookCard() {
             <div className="mt-[10.1px] mr-1 align-top">
               <span>닉네임</span>
             </div>
-            <div className="mt-[15.1px] size-[15px] justify-center rounded-full border-1"></div>
+            {/* 배지 에리어 */}
+            <div className="mt-[15.1px] size-[15px] justify-center overflow-hidden rounded-full border-1"></div>
           </div>
           <div className="mt-[15px] truncate">
             <span>게시글 제목</span>
@@ -18,6 +19,7 @@ export default function BookCard() {
           <div className="mt-[15px] line-clamp-2">
             세상이 사랑하지 않았고 세상이 존중하지 않았지만 그저 숨쉬고 살아있는
           </div>
+          {/* 좋아요, 댓글 */}
           <div className="absolute bottom-3 left-3 flex">
             <div className="mr-[8px]">
               <span className="text-[16px]">♥</span>
