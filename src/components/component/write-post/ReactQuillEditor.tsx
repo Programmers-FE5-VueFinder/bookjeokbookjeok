7d7118f2553864e108ee388cecef6b3b359da447
JUNHORANG <sjsk3358@naver.com>
2025-06-09 11:43:40 +0900

    write-post 브랜치 생성

---
+++ src/components/component/write-post/ReactQuillEditor.tsx
@@ -0,0 +1,12 @@
+// import React, { useMemo, useCallback, memo } from 'react';
+// import styles from './quillEditor.module.css';
+import ReactQuill from 'react-quill-new';
+import 'react-quill/dist/quill.snow.css';
+
+export default function ReactQuillEditor() {
+  return (
+    <>
+      <ReactQuill />
+    </>
+  );
+}
