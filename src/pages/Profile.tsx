ffca495b4aa4b9f96e6d66c0291a672e8b796bd3
SeolWolWha <leeyoon97@naver.com>
2025-06-10 21:30:17 +0900

    💄 style: 프로필 폰트 적용

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,3 @@
-import BookCard from '../components/common/BookCard';
 import { useState } from 'react';
 import { LuPencil } from 'react-icons/lu';
 import SettingModal from '../components/component/MyPage/SettingModal';
@@ -6,6 +5,10 @@ import { twMerge } from 'tailwind-merge';
 import ProfileImg from '../components/component/MyPage/ProfileImg';
 import { useProfileStore } from '../store/profileStore';
 import { useAuthStore } from '../store/authStore';
+import DiaryArea from '../components/component/MyPage/DiaryArea';
+import FreeChannelArea from '../components/component/MyPage/FreeChannelArea';
+import BookClubArea from '../components/component/MyPage/BookClubArea';
+import BookMarkArea from '../components/component/MyPage/BookMarkArea';
 
 export default function Profile() {
   const [openSetting, setOpenSetting] = useState<boolean>(false);
@@ -25,13 +28,11 @@ export default function Profile() {
       <div>
         {/* 프로필 에리어 */}
         <div className="relative flex h-[350px] content-center justify-center shadow shadow-gray-200">
-          <div className="mt-[52px] flex flex-col items-center justify-center text-center">
+          <div className="flex flex-col items-center justify-center text-center">
             {/* 프로필 이미지 */}
             <div className="relative size-[100px]">
               <ProfileImg
-                src={
-                  profileImg || session?.user.user_metadata.avatar_url
-                }
+                src={profileImg || session?.user.user_metadata.avatar_url}
               />
               <button
                 className="absolute top-0 right-1 flex size-[25px] cursor-pointer items-center justify-center rounded-full border-3 border-white bg-gray-100"
@@ -43,19 +44,23 @@ export default function Profile() {
               </button>
             </div>
             <div className="mt-[14px] grid">
-              <span>
+              <span className="text-[16px] font-semibold">
                 {session?.user.user_metadata.name}
               </span>
-              <span>내 설명</span>
+              <span className="text-[14px]">내 설명</span>
             </div>
-            <div className="flex">
+            <div className="mt-[26px] flex">
               <div className="mr-[25px]">
-                <span className="mr-[8px]">팔로잉</span>
-                <span>000</span>
+                <span className="mr-[8px] text-[16px] font-semibold">
+                  팔로워
+                </span>
+                <span className="text-[16px]">000</span>
               </div>
               <div>
-                <span className="mr-[8px]">팔로워</span>
-                <span>000</span>
+                <span className="mr-[8px] text-[16px] font-semibold">
+                  팔로잉
+                </span>
+                <span className="text-[16px]">000</span>
               </div>
             </div>
           </div>
@@ -78,16 +83,10 @@ export default function Profile() {
           </div>
         </div>
         <div className="flex items-center justify-center bg-[#FAFAFA]">
-          <div className="grid gap-[28px] p-[100px] md:grid-cols-2 lg:grid-cols-4">
-            {content}
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
-            <BookCard />
-          </div>
+          {content === '다이어리' ? <DiaryArea /> : null}
+          {content === '자유채널' ? <FreeChannelArea /> : null}
+          {content === '독서모임' ? <BookClubArea /> : null}
+          {content === '북마크' ? <BookMarkArea /> : null}
         </div>
       </div>
       {openSetting ? (
