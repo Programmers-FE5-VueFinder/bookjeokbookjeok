d897e0d2ed12d5f6643be832de53f2c4da1581cb
ty <composedkim@naver.com>
2025-06-10 10:50:43 +0900

    ✨feat: 선택한 도서 이미지 추가

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react';
 import BookSearchModal from '../components/component/BookSearchModal';
+import type { BookDetail } from '../types/book';
 
 export default function CreatePost() {
   const [showModal, setShowModal] = useState(false);
+  const [selectedBook, setSeletedBook] = useState<BookDetail | null>(null); //도서 정보
 
   const onClose = () => setShowModal(false);
   return (
     <>
       <h1>CreatePost Component</h1>
 
+      {/* 책 입력 버튼 */}
       <div className="bg-amber-200" onClick={() => setShowModal(true)}>
         모달 오픈
       </div>
-      <BookSearchModal showModal={showModal} onClose={onClose} />
+      {/* 책 검색 모달 */}
+      <BookSearchModal
+        showModal={showModal}
+        onClose={onClose}
+        setSeletedBook={setSeletedBook}
+      />
+      {/* 선택한 책 이미지 */}
+      {selectedBook && <img src={selectedBook.cover} />}
     </>
   );
 }
