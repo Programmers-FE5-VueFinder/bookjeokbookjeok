f2cc7249f62340bee9e0bd3f8c169972fae0c9a0
ty <composedkim@naver.com>
2025-06-10 15:19:06 +0900

    💩poop: 불필요한 코드 삭제

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,10 +1,6 @@
 import WritePost from '../components/component/write-post/WritePost';
 
 export default function CreatePost() {
-  const [showModal, setShowModal] = useState(false);
-  const [selectedBook, setSeletedBook] = useState<BookDetail | null>(null); //도서 정보
-
-  const onClose = () => setShowModal(false);
   return (
     <>
       <WritePost />
