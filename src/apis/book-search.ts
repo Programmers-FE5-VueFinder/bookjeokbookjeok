c31940b7a1200a783496e034227d426776a0418b
ty <composedkim@naver.com>
2025-06-12 12:26:35 +0900

    ✨feat: 연관 장르 도서 추천 api 추가

--- src/apis/book-search.ts
+++ src/apis/book-search.ts
@@ -4,9 +4,6 @@ const TTBKey = import.meta.env.VITE_TTB_KEY;
 
 const baseParams = {
   ttbkey: TTBKey,
-  Cover: 'Big',
-  MaxResults: 10,
-  start: 1,
   SearchTarget: 'Book',
   output: 'JS',
   Version: '20131101',
@@ -17,7 +14,25 @@ export const searchBooks = async (query: string) => {
   const params = {
     ...baseParams,
     Query: query,
+    MaxResults: 10,
+    Cover: 'Big',
+    start: 1,
     QueryType: 'Title',
+  };
+
+  const { data } = await axios.get(url, { params });
+  return data.item || [];
+};
+
+export const getRelatedBooks = async (categoryId: number, page: number) => {
+  const url = `/api/ttb/api/ItemList.aspx`;
+  const params = {
+    ...baseParams,
+    Cover: 'MidBig',
+    QueryType: 'ItemEditorChoice',
+    MaxResults: 9,
+    start: page,
+    CategoryId: categoryId,
   };
 
   const { data } = await axios.get(url, { params });
