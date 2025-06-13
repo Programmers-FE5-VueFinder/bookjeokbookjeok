423bf0b71c20eaa556143ebc890fdc1f6aeeb194
ty <composedkim@naver.com>
2025-06-13 12:35:42 +0900

    ✨feat: 도서와 관련된 포스트 목록 5개씩 무한 스크롤로 가져오는 기능 추가

--- src/components/component/book-detail/BookPost.tsx
+++ src/components/component/book-detail/BookPost.tsx
@@ -1,71 +1,129 @@
+import { useCallback, useEffect, useState } from 'react';
 import { FaRegComment, FaRegHeart } from 'react-icons/fa';
+import { getBookPost } from '../../../apis/post';
+import { useInfiniteScroll } from '../../../hooks/useInfiniteScroll';
+import Snackbar from '@mui/material/Snackbar';
 
-const posts = [
-  {
-    id: 1,
-    name: '김뿡빵뺑',
-    title: '제목이 뭘까요??',
-    date: '2025년 05월 21일',
-    text: '나는 완벽하지도 않은데 게으른 사람이다. 하지만 욕심은 많다. 나는 욕심 많은 게으른 나태한 사람이다.나는 완벽하지도 않은데 게으른 사람이다. 하지만 욕심은 많다. 나는 욕심 많은 게으른 나태한 사람이다.',
-  },
-  {
-    id: 2,
-    name: '김뿡빵뺑',
-    title: '제목이 뭘까요??',
-    date: '2025년 05월 21일',
-    text: '나는 완벽하지도 않은데 게으른 사람이다. 하지만 욕심은 많다. 나는 욕심 많은 게으른 나태한 사람이다.',
-  },
-  {
-    id: 3,
-    name: '김뿡빵뺑',
-    title: '제목이 뭘까요??',
-    date: '2025년 05월 21일',
-    text: '나는 완벽하지도 않은데 게으른 사람이다. 하지만 욕심은 많다. 나는 욕심 많은 게으른 나태한 사람이다.',
-  },
-  {
-    id: 4,
-    name: '김뿡빵뺑',
-    title: '제목이 뭘까요??',
-    date: '2025년 05월 21일',
-    text: '나는 완벽하지도 않은데 게으른 사람이다. 하지만 욕심은 많다. 나는 욕심 많은 게으른 나태한 사람이다.',
-  },
-];
+interface Post {
+  id: string;
+  title: string;
+  body: string;
+  created_at: string;
+  user_id: string;
+  profile: {
+    name: string;
+    image: string | null;
+  };
+}
 
-export default function BookPost() {
+export default function BookPost({ isbn }: { isbn: string }) {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+  const fetchPosts = useCallback(async () => {
+    if (isLoading || !hasMore) return;
+    setIsLoading(true);
+    try {
+      const from = page * 5;
+      const to = from + 4;
+      const newPosts = await getBookPost(isbn, from, to);
+
+      setPosts((prev) => [...prev, ...newPosts]);
+      setPage((prev) => prev + 1);
+
+      if (newPosts.length < 5) {
+        setHasMore(false);
+      }
+    } catch (error) {
+      console.error('리뷰 불러오기 실패:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [isbn, page, hasMore, isLoading]);
+
+  const loadMoreRef = useInfiniteScroll({
+    hasMore,
+    isLoading,
+    onIntersect: fetchPosts,
+  });
+
+  useEffect(() => {
+    if (!hasMore && !isLoading && posts.length > 0) {
+      setSnackbarOpen(true);
+    }
+  }, [hasMore, isLoading, posts.length]);
+
+  useEffect(() => {
+    setPosts([]);
+    setPage(0);
+    setHasMore(true);
+  }, [isbn]);
   return (
     <>
-      <div className='h-full max-h-[67vh] overflow-auto custom-scrollbar rounded-[10px] '>
-        <p className='text-center font-semibold text-[20px] my-[30px]'>포스트</p>
-        {posts.map((post) => (
-          <div
-            key={post.id}
-            className=' max-h-full w-full flex-col justify-center p-[15px] pl-[20px]  text-[16px]  border-t border-t-[#D8D8D8]'
-          >
-            <div className='flex mb-[10px]'>
-              <div className='flex items-center gap-[10px] cursor-pointer'>
-                <p className='text-[16px] text-[#333333] mt-[2px]'>{post.name}</p>
-                <div className='w-[30px] h-[30px] rounded-4xl bg-gray-500'></div>
-              </div>
-            </div>
-            <div className='font-medium flex flex-col gap-[10px] cursor-pointer '>
-              <span className='font-semibold text-[16px]'>{post.title}</span>
-              <span className=' text-[16px] line-clamp-2'>{post.text}</span>
-              <div className='flex justify-between font-medium text-[14px]'>
-                <div className='flex  items-center justify-center gap-[4px]'>
-                  <span>
-                    <FaRegHeart fontSize='small' className='mt-[2px]' />
-                  </span>
-                  <span className='mr-[4px]'>34</span>
-                  <span>
-                    <FaRegComment fontSize='small' className='mt-[2px]' />
-                  </span>
-                  <span>6</span>
+      <div className="custom-scrollbar h-full max-h-[calc(75vh-60px)] overflow-auto rounded-[10px]">
+        <p className="border-b border-b-[#D8D8D8] py-[30px] text-center text-[20px] font-semibold">
+          포스트
+        </p>
+        {posts.length === 0 && !isLoading ? (
+          <p className="flex h-[400px] items-center justify-center border-t border-t-[#D8D8D8] py-[30px] text-2xl text-gray-500">
+            등록된 포스트가 없습니다.
+          </p>
+        ) : (
+          posts.map((post) => (
+            <div
+              key={post.id}
+              className="max-h-full w-full flex-col justify-center border-b border-b-[#D8D8D8] p-[15px] pl-[20px] text-[16px]"
+            >
+              {/* 작성자 */}
+              <div className="mb-[10px] flex">
+                <div className="flex cursor-pointer items-center gap-[10px]">
+                  <p className="text-[16px] text-[#333333]">
+                    {post.profile.name}
+                  </p>
+                  <img
+                    src={post.profile.image!}
+                    alt="작성자 프로필"
+                    className="mt-[5px] h-[16px] w-[16px] rounded-full object-cover"
+                  />
                 </div>
-                <div>{post.date}</div>
+              </div>
+              <div className="flex cursor-pointer flex-col gap-[10px] font-medium">
+                <span className="line-clamp-1 text-[16px] font-semibold">
+                  {post.title}
+                </span>
+                <span className="line-clamp-2 text-[16px]">{post.body}</span>
+                <div className="flex justify-between text-[14px] font-medium">
+                  <div className="flex items-center justify-center gap-[4px]">
+                    <span>
+                      <FaRegHeart fontSize="small" className="mt-[2px]" />
+                    </span>
+                    <span className="mr-[4px]">34</span>
+                    <span>
+                      <FaRegComment fontSize="small" className="mt-[2px]" />
+                    </span>
+                    <span>6</span>
+                  </div>
+                  <div>{post.created_at}</div>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
+        <div ref={loadMoreRef} className="h-[1px]" />
+
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={2000}
+          onClose={handleSnackbarClose}
+          message="더 이상 불러올 포스트가 없습니다."
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        />
       </div>
     </>
   );
