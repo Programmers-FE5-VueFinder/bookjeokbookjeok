aad17448db1f9c6d33c91e64dca495b31111e63f
ty <composedkim@naver.com>
2025-06-13 17:22:58 +0900

    🐛fix: gap 음수인 경우 0초 임시로 처리

--- src/utils/format-time.ts
+++ src/utils/format-time.ts
@@ -5,6 +5,7 @@ export default function getElapsedTime(createdAt: string) {
   const writeTime = dayjs(createdAt);
 
   const gap = now.diff(writeTime, 's');
+  if (gap < 0) return `1초 전`;
   if (gap < 60) return `${gap}초 전`;
   if (gap < 3600) return `${Math.floor(gap / 60)}분 전`;
   if (gap < 86400) return `${Math.floor(gap / 3600)}시간 전`;
