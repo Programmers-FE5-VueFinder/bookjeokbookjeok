14ddec18144fa05440bd7f58be355b6ff654c8da
JUNHORANG <sjsk3358@naver.com>
2025-06-14 09:12:45 +0900

    라우트 수정

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,10 +1,8 @@
-import SignUpModal from '../components/common/SignUpModal';
 import WritePost from '../components/component/write-post/WritePost';
 
 export default function CreatePost() {
   return (
     <>
-      <SignUpModal />
       <WritePost />
     </>
   );
