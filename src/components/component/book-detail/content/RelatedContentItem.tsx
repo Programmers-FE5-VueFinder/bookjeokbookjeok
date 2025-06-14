9715bb1132548717f34c2460b601a60baafe8f7d
ty <composedkim@naver.com>
2025-06-14 16:12:59 +0900

    💄 style: hover 시에만 중앙을 기준으로 변경

--- src/components/component/book-detail/content/RelatedContentItem.tsx
+++ src/components/component/book-detail/content/RelatedContentItem.tsx
@@ -21,10 +21,10 @@ export function RelatedContentItem({
     >
       <img
         src={book.cover}
-        className={`origin-center transition-all duration-600 ease-in-out ${
+        className={`origin-left transition-all duration-600 ease-in-out ${
           isOpen && selectedBook?.isbn13 === book.isbn13
             ? 'pointer-events-none z-2 mt-[-50px] h-[220px] w-[150px] translate-x-[-5px] scale-300 -rotate-y-180 rounded-2xl opacity-0'
-            : 'h-[220px] w-[150px] cursor-pointer rounded-sm hover:-translate-y-2 hover:scale-110 hover:shadow-lg'
+            : 'h-[220px] w-[150px] cursor-pointer rounded-sm hover:origin-center hover:-translate-y-2 hover:scale-110 hover:shadow-lg'
         }`}
       />
       <div className="text-center text-[14px] font-semibold">{book.title}</div>
