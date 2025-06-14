3bd0b766349e501929627facba1b7c0b06305b3a
ty <composedkim@naver.com>
2025-06-15 04:29:21 +0900

    ✨ feat: ToastContaniner 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { ToastContainer } from 'react-toastify';
 import Router from './routes';
 
 export default function App() {
   return (
     <>
       <Router />
+
+      <ToastContainer
+        position="top-center"
+        autoClose={1500}
+        hideProgressBar={false}
+        closeOnClick
+        theme="light"
+        progressClassName="custom-progress"
+      />
     </>
   );
 }
