525e789bc741fc216f658917d088298c7242b0c3
seoyeoxxlee <seoyeonglee@umail.ucsb.edu>
2025-06-17 05:42:59 +0900

    [merge] 중간 병합

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -1,12 +1,36 @@
-import { useState } from 'react';
-import { IoSearch } from 'react-icons/io5';
 import { twMerge } from 'tailwind-merge';
+import { fetchUser } from '../apis/user';
+import { IoSearch } from 'react-icons/io5';
+import { useEffect, useState } from 'react';
+import UserCard from '../components/common/UserCard';
 // import BookCard from '../components/common/BookCard';
-// import UserCard from '../components/common/UserCard';
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import UserCardSkeleton from '../components/common/UserCardSkeleton';
 
 export default function SearchResult() {
-  const buttonName = ['통합 검색', '게시물', '사용자'];
+  const [users, setUsers] = useState<User[]>([]);
+  const buttonName = ['통합 검색', '사용자', '게시물'];
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedBtn, setSelectedBtn] = useState<string>('통합 검색');
+  
+  // const [searchKeyword, setSearchKeyword] = useState('');
+  // const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+
+
+  useEffect(() => {
+    const getUsers = async () => {
+      setIsLoading(true);
+      try {
+        const fetchUsers = await fetchUser();
+        setUsers(fetchUsers || []);
+      } catch (error) {
+        console.error('유저 불러오기 실패:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getUsers();
+  }, []);
 
   const handleContentButton = (e: React.MouseEvent<HTMLButtonElement>) => {
     const { name } = e.currentTarget;
@@ -15,7 +39,7 @@ export default function SearchResult() {
 
   return (
     <>
-      <div className="justify-center, flex flex-col items-center border-1">
+      <div className="justify-center, flex flex-col items-center">
         <div className="relative flex h-[230px] w-full flex-col items-center justify-center gap-[27px] pb-[40px] shadow shadow-gray-200">
           <h1 className="textH1">검색</h1>
           <div className="relative flex rounded-sm border-2 border-[#d2d2d2]">
@@ -48,32 +72,59 @@ export default function SearchResult() {
             </div>
           </div>
         </div>
+
         <div className="flex w-full flex-col items-center justify-center bg-[#FAFAFA]">
-          <div className="m-[100px] max-w-[1196px]">
-            <span className="textT2">사용자</span>
-            <div className="flex flex-col items-center justify-center">
-              <div className="mt-[26px] grid gap-[28px] md:grid-cols-2 lg:grid-cols-4">
-                {/* <UserCard /> */}
-                {/* <BookCard />
-                <BookCard />
-                <BookCard /> */}
+          {/* 사용자 영역 */}
+          {(selectedBtn === '통합 검색' || selectedBtn === '사용자') && (
+            <div className="m-[100px] max-w-[1200px]">
+              <div className='flex justify-between items-center'>
+                <span className="textT2">사용자</span>
+                {selectedBtn === '통합 검색' && (
+                  <MdOutlineKeyboardArrowRight 
+                    size={30} 
+                    color="#1C1C1C" 
+                    className='cursor-pointer'
+                    onClick={() => setSelectedBtn('사용자')}
+                  />
+                )}
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="mt-[30px] grid gap-[47px] md:grid-cols-2 lg:grid-cols-6">
+                  {isLoading
+                    ? Array.from({ length: 6 }).map((_, idx) => (
+                        <UserCardSkeleton key={idx} />
+                      ))
+                    : users.map((user) => (
+                        <UserCard key={user.id} user={user} />
+                      ))}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="m-[50px] max-w-[1196px]">
-            <span className="textT2">게시물</span>
-            <div className="flex flex-col items-center justify-center">
-              <div className="mt-[26px] grid gap-[28px] md:grid-cols-2 lg:grid-cols-4">
-                {/* <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard />
-                <BookCard /> */}
+          )}
+
+          {/* 게시물 영역 */}
+          {(selectedBtn === '통합 검색' || selectedBtn === '게시물') && (
+            <div className="m-[50px] max-w-[1200px]">
+              <div className='flex justify-between items-center'>
+                <span className="textT2">게시물</span>
+                {selectedBtn === '통합 검색' && (
+                  <MdOutlineKeyboardArrowRight 
+                    size={30} 
+                    color="#1C1C1C" 
+                    className='cursor-pointer'
+                    onClick={() => setSelectedBtn('게시물')}
+                  />
+                )}
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <div className="mt-[26px] grid gap-[28px] md:grid-cols-2 lg:grid-cols-4">
+                  {/* <BookCard /> */}
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </>
