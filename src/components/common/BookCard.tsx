3ca891f2840b9cc13e2101073722c8712f3f1b5a
seoyeoxxlee <seoyeonglee@umail.ucsb.edu>
2025-06-16 10:12:07 +0900

    BookCard 컴포넌트 수정

--- src/components/common/BookCard.tsx
+++ src/components/common/BookCard.tsx
@@ -1,4 +1,4 @@
-import { FaRegComment, FaRegHeart } from 'react-icons/fa';
+import { FaComment, FaHeart } from "react-icons/fa";
 import type { BookCardProps } from '../../types/type';
 
 export default function BookCard({
@@ -15,7 +15,7 @@ export default function BookCard({
   return (
     <>
       <div
-        className="relative h-[440px] w-[278px] flex-col justify-center rounded-[10px] bg-white text-center text-[16px]"
+        className="relative h-auto w-[278px] flex-col justify-center rounded-[10px] bg-white text-center text-[16px]"
         style={{
           boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)',
         }}
@@ -31,42 +31,45 @@ export default function BookCard({
             <span>이미지 없음</span>
           )}
         </div>
-        <div className="p-[13px] text-start">
-          <div className="bg-amber-100 flex items-center gap-x-[6px]">
+        
+        <div className="flex flex-col h-[193px] px-[13px] pt-[13px] text-start">
+
+
+          <div className="flex h-[40px] items-center gap-x-[4px]">
             <img 
               src={profileImage ?? '이미지 없음'}
               alt="프로필사진" 
               className=" w-auto h-[25px] rounded-full"
             />
-            <div className="flex items-center">
+            <div className="flex items-center w-full">
               <div className="text-[16px] font-semibold">{nickname}</div>
             </div>
           </div>
 
-          <div className="mt-[15px] truncate text-[18px] font-bold">
+          <div className="w-[253px] h-[40px] line-clamp-1 bg-red-200 flex items-center mt-[4px] truncate text-[16px] font-bold">
             <span>{title}</span>
           </div>
-          <div className="mt-[15px] line-clamp-2">{body}</div>
+          <div className="w-auto h-[55px] my-[8px] text-[16px] bg-amber-400 line-clamp-2">
+            {body}
+          </div>
+
+
+
           {/* 좋아요, 댓글 */}
-          <div className="absolute bottom-0 left-0 flex size-[12px] pb-[30px] pl-[13px]">
-            <div className="mr-[8px] flex items-center space-x-1">
-              <span>
-                <FaRegHeart fontSize="small" />
-              </span>
-              <span>{likes}</span>
+
+          <div className="flex justify-between w-[254px] h-[30px] mb-[7px] bg-amber-200 bottom-0">
+              <div className="bg-red-200 flex items-center w-fit gap-x-[8px]">
+                <div className="flex items-center space-x-1">
+                  <FaHeart fontSize="small" color="#353535" />
+                  <span className="text-[#353535]">{likes}</span>
+                </div>
+                <div className="flex items-center space-x-1">
+                  <FaComment fontSize="small" color="#353535" />
+                  <span className="text-[#353535]">{comments}</span>
+                </div>
+              </div>
+              <div className="flex items-center bg-blue-200">{createdAt}</div>
             </div>
-            <div className="flex items-center space-x-1">
-              <span>
-                <FaRegComment fontSize="small" />
-              </span>
-              <span>{comments}</span>
-            </div>
-          </div>
-          <div>
-            <span className="absolute right-0 bottom-0 pr-[13px] pb-[7px]">
-              {createdAt}
-            </span>
-          </div>
         </div>
       </div>
     </>
