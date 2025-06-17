96910a7e6734dce576faadc47a13f60bab2bd104
ty <composedkim@naver.com>
2025-06-17 14:51:26 +0900

    ✨ feat: local 시간 변환 추가

--- src/utils/format-time.ts
+++ src/utils/format-time.ts
@@ -1,7 +1,20 @@
 import dayjs from 'dayjs';
 
-export default function getElapsedTime(createdAt: string) {
+export function getElapsedTime(createdAt: string) {
   const now = dayjs().add(9, 'hour');
+  const writeTime = dayjs(createdAt);
+
+  const gap = now.diff(writeTime, 's');
+
+  if (gap < 119) return `1분 전`;
+  if (gap < 3600) return `${Math.floor(gap / 60)}분 전`;
+  if (gap < 86400) return `${Math.floor(gap / 3600)}시간 전`;
+
+  return writeTime.format('YYYY.MM.DD');
+}
+
+export function getLocalElapsedTime(createdAt: string) {
+  const now = dayjs();
   const writeTime = dayjs(createdAt);
 
   const gap = now.diff(writeTime, 's');
