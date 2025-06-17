a7d0875ceebe2713bde7b071ea318d412ff45aea
seoyeoxxlee <seoyeonglee@umail.ucsb.edu>
2025-06-17 10:27:37 +0900

    [merge] 인기 다이어리 리스트 수정

--- src/components/component/Home/PopularDiaryCard.tsx
+++ src/components/component/Home/PopularDiaryCard.tsx
@@ -1,33 +1,37 @@
 import type { PopularDiaryCardProps } from "../../../types/type";
 
-const genreImageMap: Record<string, { image: string; label: string }> = {
-  '국내도서>인문학>교양 인문학': {
+const genreImageList = [
+  {
+    match: (genre: string) => genre.includes('인문'),
     image: "/images/home_humanities_illust.png",
-    label: "인문학",
+    label: "인문",
   },
-  novel: {
+  {
+    match: (genre: string) => genre.includes('소설'),
     image: '/images/home_novel_illust.png',
     label: '소설',
   },
-  education: {
+  {
+    match: (genre: string) => genre.includes('교육'),
     image: '/images/home_edu_illust.png',
     label: '교육',
   },
-  development: {
+  {
+    match: (genre: string) => genre.includes('개발') || genre.includes('자기계발'),
     image: '/images/home_dev_illust.png',
     label: '자기 개발',
   },
-  humanities: {
-    image: '/images/home_humanities_illust.png',
-    label: '인문',
-  },
-};
+];
 
   
 export default function PopularDiaryCard({genre, title, content}: PopularDiaryCardProps) {
-  const genreData = genre && genreImageMap[genre];
-
-  if (!genreData) return null; // 장르가 유효하지 않으면 렌더링 안 함
+  const getGenreImage = (genre: string) => {
+    return genreImageList.find((item) => item.match(genre)) ?? null;
+  };
+  if (!genre) return null; // 장르값 유효하지 않으면 렌더링 안 함
+  
+  const genreData = getGenreImage(genre);
+  if (!genreData) return null;
 
   return (
     <div
