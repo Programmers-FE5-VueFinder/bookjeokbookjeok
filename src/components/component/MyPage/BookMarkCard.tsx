55c6a9bb0fc646b46f462ab4a24450a13b7a93b9
SeolWolWha <leeyoon97@naver.com>
2025-06-18 22:30:30 +0900

    💄 style: 프로필 뒤쪽 노란 배경 제거

--- src/components/component/MyPage/BookMarkCard.tsx
+++ src/components/component/MyPage/BookMarkCard.tsx
@@ -100,7 +100,7 @@ export default function BookMarkCard({
           ) : null}
         </div>
         <div className="p-[13px] text-start">
-          <div className="flex items-center gap-x-[6px] bg-amber-100">
+          <div className="flex items-center gap-x-[6px]">
             <div className="size-[25px] overflow-hidden rounded-full">
               <ProfileImg id={id} />
             </div>
