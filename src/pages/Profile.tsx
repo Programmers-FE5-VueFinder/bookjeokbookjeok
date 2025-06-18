d6c6e3bc7afbc5ced21a00ee46ebfaab3cc07a42
SeolWolWha <leeyoon97@naver.com>
2025-06-18 20:46:45 +0900

    💄 style: 북클럽 생성일 기준 내림차순 정렬

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -220,12 +220,14 @@ export default function Profile() {
       const myBookClub = async () => {
         const { data: book_club } = await supabase
           .from('book_club')
-          .select('*');
+          .select('*')
+          .order('created_at', { ascending: false });
 
         const { data: book_club_member } = await supabase
           .from('book_club_member')
           .select('*')
-          .eq('user_id', userId!);
+          .eq('user_id', userId!)
+          .order('created_at', { ascending: false });
 
         const clubId = book_club_member?.filter((id) => id.user_id === userId);
         const clubIds: string[] = [];
